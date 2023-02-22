8076a69e446731783a61437386625b570766272c
Alex Chiu <alexdchiu@gmail.com>
2023-02-22 10:17:41 -0500

    changed packagejson for deploy

--- front-end/.next/static/chunks/webpack.js
+++ front-end/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1710291c0f50f3f"; }
+/******/ 		__webpack_require__.h = function() { return "8ec9ddfad64eb693"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
