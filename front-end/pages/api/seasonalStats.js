d18b795d85bbe626e452ffdcf2696ebb27eb9f78
Alex Chiu <alexdchiu@gmail.com>
2023-02-22 11:27:09 -0500

    testing to get api to work

--- front-end/pages/api/seasonalStats.js
+++ front-end/pages/api/seasonalStats.js
@@ -7,13 +7,12 @@ const teamStats = async (req, res) => {
   const season_year = '2022'
   const season_type = 'REG'
   const team_id = '583ec9d6-fb46-11e1-82cb-f4ce4684ea4c'
-  const key = `${process.env.NEXT_PUBLIC_SPORTSRADAR_API_KEY}`
 
   const delay = ms => new Promise(res => setTimeout(res, ms));
 
   await delay(1000)
 
-  const url = `http://api.sportradar.us/nba/trial/v7/${language_code}/seasons/${season_year}/${season_type}/teams/${team_id}/statistics.json?api_key=${key}`
+  const url = `http://api.sportradar.us/nba/trial/v7/${language_code}/seasons/${season_year}/${season_type}/teams/${team_id}/statistics.json?api_key=${process.env.NEXT_PUBLIC_SPORTSRADAR_API_KEY}`
 
   console.log('pppp', url)
 
